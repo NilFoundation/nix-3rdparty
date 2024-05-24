e39ddf5b1caa5665459f300e011c5d8e689d81b2
Alexander Evgin <aleasims@gmail.com>
2024-05-24 15:34:14 +0400

    Pin version of EVMC to out tested revision
    Previous revision was not working for some reason

--- nix/evmc/evmc.nix
+++ nix/evmc/evmc.nix
@@ -2,13 +2,12 @@
 
 stdenv.mkDerivation rec {
   name = "evmc";
-  version = "11.0.1";
 
   src = fetchFromGitHub {
     owner = "ethereum";
     repo = "evmc";
-    rev = "v${version}";
-    hash = "sha256-BSnKW35VvU/5VKVB7A5TjNnj0zkR4n4Dg4YsuoAJJuU=";
+    rev = "fc86231960348790bbee8254a809bf1f9d7c8517";
+    hash = "sha256-yqEbtRA4GiQ/pjaH2+ZPfnthgt9gLPY4lWPTYbBWJhE=";
   };
 
   nativeBuildInputs = [ pkgs.cmake ];
