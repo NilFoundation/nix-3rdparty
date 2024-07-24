f7f40a18b5ce2d7acfefb9160e7e0bd56532ecc6
Andrei Malashkin <andrei.malashkin@nil.foundation>
2024-07-24 10:55:15 +0200

    use nil-fork of cmake_modules

--- nix/cmake_modules/cmake_modules.nix
+++ nix/cmake_modules/cmake_modules.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation {
   name = "cmake_modules";
 
   src = fetchFromGitHub {
-    owner = "BoostCMake";
+    owner = "NilFoundation";
     repo = "cmake_modules";
     rev = "57639741ecf018835deb97a04db2200241d7fbd3";
     hash = "sha256-/Fu+Co1W/0/c7GhLLv1Ozyw7LyezYGZtj3NODfS339o=";
